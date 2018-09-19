43b14b8c6fd8c94aa22b4062b18fbca1fe927537
peterpeiguo <peterpeiguo@gmail.com>
2018-09-18 17:05:16 -0700

    changed to OO style and removed hard coded save location

--- verses.rb
+++ verses.rb
@@ -1,47 +1,55 @@
 require 'nokogiri'
 require 'open-uri'
 
-def get_verse(book, chapter)
-	url = "http://www.kingjamesbibleonline.org/#{book}-Chapter-#{chapter}"
-	puts "#{Time.now}, downloading #{url}"
-	file = open(url)
-	contents = file.read
-	puts "#{Time.now}, downloaded #{url}"
-	document = Nokogiri::HTML(contents)
-	puts document.css("span.chapter").text.upcase
-	puts "chapter #{chapter}".upcase
-	if document.css("span.chapter").text.upcase != "chapter #{chapter}".upcase then
-		return false
+class VerseDownloader
+	def initialize(base_path)
+		@base_path = base_path
+		Dir.mkdir(File.join(@base_path, "verses")) if not Dir.exist?(File.join(@base_path, "verses"))
 	end
 
-	File.open("verses\\#{book}_#{chapter}.txt", 'w') do
-		|file|
-		file.puts "#{book} chapter #{chapter}".upcase
-		file.puts
-
-		document.css(".versehover").each do
-			|verse|
-			puts verse.parent.parent.text
-			file.puts(verse.parent.parent.text)
+	def get_verse(book, chapter)
+		url = "http://www.kingjamesbibleonline.org/#{book}-Chapter-#{chapter}"
+		puts "#{Time.now}, downloading #{url}"
+		file = open(url)
+		contents = file.read
+		puts "#{Time.now}, downloaded #{url}"
+		document = Nokogiri::HTML(contents)
+		puts document.css("span.chapter").text.upcase
+		puts "chapter #{chapter}".upcase
+		if document.css("span.chapter").text.upcase != "chapter #{chapter}".upcase then
+			return false
 		end
 
-		file.puts
+		File.open(File.join(@base_path, "verses", "#{book}_#{chapter}.txt"), 'w') do
+			|file|
+			file.puts "#{book} chapter #{chapter}".upcase
+			file.puts
+
+			document.css(".versehover").each do
+				|verse|
+				puts verse.parent.parent.text
+				file.puts(verse.parent.parent.text)
+			end
+
+			file.puts
+		end
+
+		return true
 	end
 
-	return true
-end
-
-def get_book(book)
-	chapter = 0
-	while true do
-		chapter = chapter + 1 
-		break if not get_verse(book, chapter) 
+	def get_book(book)
+		chapter = 0
+		while true do
+			chapter = chapter + 1 
+			break if not get_verse(book, chapter) 
+		end
 	end
 end
 
-Dir.mkdir("verses") if not Dir.exist?("verses")
-#get_book("Genesis")
-get_book("Exodus")
+downloader = VerseDownloader.new(Dir.pwd)
+#downloader.get_book("Genesis")
+#downloader.get_book("Exodus")
+downloader.get_book("Leviticus")
 
 =begin
 (1..28).each do
